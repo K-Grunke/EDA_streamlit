897a2ad51d72a4bade54bb9bcc033be7865191a8
K-Grunke <konrad.grunke@gmail.com>
2026-01-17 18:29:35 +0100

    budowa struktury repo, wczytanie danych

---
+++ src/load_data.py
@@ -0,0 +1,16 @@
+# load_data.py
+import pandas as pd
+
+def load_data(path: str):
+    """
+    Wczytuje plik CSV i konwertuje kolumny do odpowiednich typów.
+    """
+    df = pd.read_csv(path)
+
+    # Upewnij się, że salary_in_usd jest float
+    df['salary_in_usd'] = df['salary_in_usd'].astype(float)
+    
+    # work_year jako int
+    df['work_year'] = df['work_year'].astype(int)
+    
+    return df
